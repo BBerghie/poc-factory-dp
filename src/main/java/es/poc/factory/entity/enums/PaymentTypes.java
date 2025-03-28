8b02d3f41c40dccb4df617124453dbe15c0cbb37
bgd <bberghie@gmail.com>
2025-03-28 23:50:08 +0100

    factory poc

---
+++ src/main/java/es/poc/factory/entity/enums/PaymentTypes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package es.poc.factory.entity.enums;
+
+/**
+ *
+ * @author bgd
+ */
+public enum PaymentTypes {
+    PAYPAL, VISA, GOOGLE_PAY, APPLE_PAY
+}
